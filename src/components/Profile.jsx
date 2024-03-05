81e2283a455cd0f42fe704dc885ee3e6eebc9b3f
Tom Burns <tomb7030@gmail.com>
2024-03-05 01:15:09 +0000

    Should be able to host properly now :)

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,17 +2,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import Navbar from "./Navbar";
 import "../styles/Profile.css";
+import profileImage from "../assets/IMG_5794_x512.jpg";
 
 const Profile = () => {
   return (
     <div>
       <Navbar />
       <div className="profile-container">
-        <img
-          className="profile-image"
-          src="src\assets\IMG_5794_x512.jpg"
-          alt="Tom"
-        />
+        <img className="profile-image" src={profileImage} alt="Tom" />
         <h2>Thomas Burns</h2>
         <p>Junior Web Developer</p>
         <div className="link-container">
